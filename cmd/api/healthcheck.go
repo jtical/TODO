756b347adb61ddd6a65c922097daec23a0d843fc
Joel Ical <2005113038@ub.edu.bz>
2022-10-18 16:25:21 -0600

    healthcheck response returns in json format

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -3,12 +3,28 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
+	//create a map to hold our healthcheck data
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
+	//convert our map into a json object
+	js, err := json.Marshal(data)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The Server encountered a prolem and could not Process request", http.StatusInternalServerError)
+		return
+	}
+	//add a newline to make view on terminaL better
+	js = append(js, '\n')
+	//specify that we will serve our responses using JSON
+	w.Header().Set("Content-Type", "application/json")
+	//write the [] byte slice containing the JSON response body
+	w.Write(js)
 }
